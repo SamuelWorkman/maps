dda3b31b37a1b590e6b257f93097db5a472e7a4b
Samuel Workman <sam.workman@mail.wvu.edu>
2024-03-21 17:05:02 -0400

    Error checking with height and width ratios.

--- morgantown_wv/kontur_pop_den_morgantown_src_2023.R
+++ morgantown_wv/kontur_pop_den_morgantown_src_2023.R
@@ -25,8 +25,72 @@ admin <- st_read("kontur_boundaries_US_20230628.gpkg")
 wv_places <- places(state = "West Virginia")
 
 mo <- wv_places |> 
-  filter(NAME == "Morgantown")
+  filter(NAME == "Morgantown") |>
+  st_transform(crs = st_crs(pop))
 
 mo |> 
   ggplot() +
   geom_sf()
+
+pl_mo <- st_intersection(pop, mo)
+
+# define aspect ratio based on bounding box
+
+bb <- st_bbox(pl_mo)
+
+bottom_left <- st_point(c(bb[["xmin"]], bb[["ymin"]])) |> 
+  st_sfc(crs = st_crs(pop))
+
+bottom_right <- st_point(c(bb[["xmax"]], bb[["ymin"]])) |> 
+  st_sfc(crs = st_crs(pop))
+
+mo |> 
+  ggplot() +
+  geom_sf() +
+  geom_sf(data = bottom_left) +
+  geom_sf(data = bottom_right, color = "red")
+
+width <- st_distance(bottom_left, bottom_right)
+
+top_left <- st_point(c(bb[["xmin"]], bb[["ymax"]])) |> 
+  st_sfc(crs = st_crs(pop))
+
+height <- st_distance(bottom_left, top_left)
+
+
+# handle conditions of width or height being the longer side
+
+if (width > height) {
+  w_ratio <- 1
+  h_ratio <- height / width
+} else {
+  h_ratio <- 1
+  w_ratio <- width / height
+}
+
+# convert to raster so we can then convert to matrix
+
+size <- 5000
+
+mo_rast <- mo_rast <- st_rasterize(pl_mo, nx = floor(size * w_ratio), ny = floor(size * h_ratio))
+
+mat <- matrix(mo_rast$population, 
+              nrow = floor(size * w_ratio),
+              ncol = floor(size * h_ratio))
+
+c1 <- met.brewer("OKeeffe2")
+swatchplot(c1)
+
+texture <- grDevices::colorRampPalette(c1, bias = 2)(256)
+swatchplot(texture)
+
+# plot that 3d thing!
+
+rgl::close3d()
+
+mat |> 
+  height_shade(texture = texture) |> 
+  plot_3d(heightmap = mat,
+          zscale = 100 / 5,
+          solid = FALSE,
+          shadowdepth = 0)
